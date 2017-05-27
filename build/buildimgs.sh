3506a28f63e9646785102cd21079ab821e3e47a1
Jason Andersen <andersen.jason@gmail.com>
2017-05-27 04:53:24 -1000

    Running docker commands under sudo

--- build/buildimgs.sh
+++ build/buildimgs.sh
@@ -2,12 +2,12 @@
 # This shell script will build all the Docker images for the build server. This should be run once prior to running docker-compose.
 
 #mysql image
-docker build ./mysql/ -t build-sonardb
+sudo docker build ./mysql/ -t build-sonardb
 
 #sonarqube image
-docker build ./sonarqube/ -t build-sonar
+sudo docker build ./sonarqube/ -t build-sonar
 
 #jenkins image
-docker build ./jenkins/base-jenkins/oracle-jdk-8/ -t oracle-jdk8
-docker build ./jenkins/base-jenkins/ -t base-jenkins
-docker build ./jenkins/ -t build-jenkins
+sudo docker build ./jenkins/base-jenkins/oracle-jdk-8/ -t oracle-jdk8
+sudo docker build ./jenkins/base-jenkins/ -t base-jenkins
+sudo docker build ./jenkins/ -t build-jenkins
